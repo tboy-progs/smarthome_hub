6de70610f3ce363a56737c0581aa6f563efc5e36
tboy <gluestick829@gmail.com>
2025-05-31 13:30:15 +0900

    add: dhtセンサ用ファイルを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
-#include <Arduino.h>
+#include "M5Stack.h"
+#include "DHT.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
 }
---
+++ src/user_dht/user_dht.h
@@ -0,0 +1,4 @@
+#ifndef _USER_DHT_H_
+#define _USER_DHT_H_
+
+#endif
